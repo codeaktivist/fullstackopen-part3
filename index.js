e5844b35a5c156d507d5c8c66a6075f1e6b2b9ba
codeaktivist <codeaktivist@gmail.com>
2022-10-27 11:09:22 +0200

    Stage 3.11

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 
 const app = express()
 // app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan((tokens, request, response) => {
     return [
